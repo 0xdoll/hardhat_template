0cef2c3d2965ed4b4a1bde655835f6f7e65733fc
0xdoll <98440810+0xdoll@users.noreply.github.com>
2022-05-23 18:31:27 +0800

    update signer to use

--- scripts/test.js
+++ scripts/test.js
@@ -3,8 +3,7 @@ const { artifacts, ethers } = require("hardhat");
 (async () => {
     
     // npx hardhat run test/td.js --network <network>
-    
-    var signer = (await ethers.getSigners())[2]; // first signer.
+    var signer = (await ethers.getSigners())[0]; // first signer.
     // signer.provider = new ethers.providers.JsonRpcProvider(); // for localhost node cli, remove if no need.
     const contract_addr = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
     const tokenArtifact = await artifacts.readArtifact("Helloer");
